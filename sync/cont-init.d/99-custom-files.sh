6dca807e9bf819cb19c3fb64fe3b9eda3ea1c50b
luandro <luandro@gmail.com>
2022-04-11 22:04:42 -0300

    adiciona auto-configuração de acervo para o syncthing

--- sync/cont-init.d/99-custom-files.sh
+++ sync/cont-init.d/99-custom-files.sh
@@ -2,11 +2,8 @@
 # shellcheck shell=bash
 
 echo "=======" "Adiciona variaveis do Acervo" "====="
-mkdir -p /config/jellyfin-config
-chown -R 1000:1000 /config
+
 sed -i -r "s/#ID_PRINCIPAL/$SYNC_ID_PRINCIPAL/g" /config/config.xml
 sed -i -r "s/#NOME_PRINCIPAL/$SYNC_NOME_PRINCIPAL/g" /config/config.xml
 sed -i -r "s/#ID_ACERVO/$SYNC_ID_ACERVO/g" /config/config.xml
 sed -i -r "s/#NOME_ACERVO/$SYNC_NOME_ACERVO/g" /config/config.xml
-sed -i -r "s/#ID_CONFIG/$SYNC_ID_CONFIG/g" /config/config.xml
-sed -i -r "s/#NOME_CONFIG/$SYNC_NOME_CONFIG/g" /config/config.xml
