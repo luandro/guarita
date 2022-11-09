679fc8fd431e11a565e5af58884d7754a0bcc76e
luandro <luandro@digital-democracy.org>
2022-11-09 19:17:10 +0100

    Faz preload de imagens prontas
    * adiciona pihole-updatelists e remove scripts antigos
    
    * atualiza para v3; passa ADLISTS_URL para balena.yml; permissão 10-custom.sh para 755
    
    * apt get com limpeza e adicionar chmod +x para erro do 10-custom.sh
    
    * voltar para instalação não limpa
    
    * reverter mudança de modo do 10-custom.sh para 644
    
    * tentando consertar pihole
    
    * adiciona github action para fazer preload para pi 3 e 4
    
    * muda trigger da action para push
    
    * remove nome da placa do fleet para preload
    
    * adiciona --dont-check-arch a action
    
    * cria tag no github e faz upload dos zips
    
    * muda GITHUB_TOKEN por PAT na actions
    
    * adiciona action para gerar notas de release

--- pihole/cont-init.d/10-custom.sh
+++ pihole/cont-init.d/10-custom.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bash
+#!/command/with-contenv bash
 # shellcheck shell=bash
 
 set -e
@@ -7,8 +7,8 @@ set -e
 echo "=======" "Setando senha do Pihole" "====="
 pihole -a -p "${WEBPASSWORD}" || true
 # TEMPORÁRIO: remover nos próximos releases
-echo "=======" "Removendo bloqueio e desabilitando DHCP" "====="
-# pihole --regex -d '.*' # Pode estar dando problema
+echo "=======" "desabilitando DHCP" "====="
+# pihole --regex -d '.*' # isso era preciso para limpar regex antigos
 pihole -a disabledhcp
 
 # SET DOMAIN FOR LOCAL SERVICES
@@ -17,42 +17,6 @@ DOMAIN="${LOCAL_DOMAIN:=nhandeflix.com}"
 IP="${LOCAL_IP:=192.168.1.13}"
 
 echo "address=/$DOMAIN/$IP" > /etc/dnsmasq.d/03-custom-dns.conf
-
-# Add Youtube add-blocking
-# TODO: Analize if lists are safe and useful:
-# https://github.com/kboghdady/youTube_ads_4_pi-hole
-# https://github.com/blocklistproject/Lists
-# https://github.com/lightswitch05/hosts
-# https://github.com/jacklul/pihole-updatelists
-echo "=======" "Adiciona regras de bloqueio para Youtube" "====="
-# This script will fetch the Googlevideo ad domains and append them to the Pi-hole block list.
-# Run this script daily with a cron job (don't forget to chmod +x)
-# More info here: https://discourse.pi-hole.net/t/how-do-i-block-ads-on-youtube/253/136
-# Original url: https://gist.github.com/ErikFontanel/4ee1ab393b119690a293ba558976b113
-
-# File to store the YT ad domains
-FILE=/etc/pihole/youtube.hosts
-
-# Fetch the list of domains, remove the ip's and save them
-curl 'https://api.hackertarget.com/hostsearch/?q=googlevideo.com' \
-| awk -F, 'NR>1{print $1}' \
-| grep -vE "redirector|manifest" > $FILE
-
-# Replace r*.sn*.googlevideo.com URLs to r*---sn-*.googlevideo.com
-# and add those to the list too
-cat $FILE | sed -r 's/(^r[[:digit:]]+)(\.)(sn)/\1---\3-/' >> $FILE
-
-# Scan log file for previously accessed domains
-grep '^r.*googlevideo\.com' /var/log/pihole*.log \
-| awk '{print $8}' \
-| grep -vE "redirector|manifest" \
-| sort | uniq >> $FILE
-
-# Add to Pi-hole adlists if it's not there already
-if ! grep $FILE < /etc/pihole/adlists.list; then echo "file://$FILE" >> /etc/pihole/adlists.list; fi;
-
-# Add pornography list to pihole
-
 
 # Show interfaces
 while [ -z "$(ip -o -4 addr show dev "${INTERFACE}")" ]
