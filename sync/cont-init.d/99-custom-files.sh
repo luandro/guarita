8775f2228968948869a8f6ce33f132461f79ca25
Luandro <luandro@digital-democracy.org>
2023-11-02 14:29:19 -0300

    dev: update syncthing build

--- sync/cont-init.d/99-custom-files.sh
+++ sync/cont-init.d/99-custom-files.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-echo "=======" "Adiciona variaveis do Acervo" "====="
+file=/config/firstboot
 
-sed -i -r "s/#ID_PRINCIPAL/$SYNC_ID_PRINCIPAL/g" /config/config.xml
-sed -i -r "s/#NOME_PRINCIPAL/$SYNC_NOME_PRINCIPAL/g" /config/config.xml
-sed -i -r "s/#ID_ACERVO/$SYNC_ID_ACERVO/g" /config/config.xml
-sed -i -r "s/#NOME_ACERVO/$SYNC_NOME_ACERVO/g" /config/config.xml
+if [ ! -e "$file" ]; then
+    echo "First run!"
+    echo "================================"
+    echo "=======" "Adding variables" "====="
+    sed -i -r "s/#ID_PRINCIPAL/$SYNC_ID_PRINCIPAL/g" /config/config.xml
+    sed -i -r "s/#NOME_PRINCIPAL/$SYNC_NOME_PRINCIPAL/g" /config/config.xml
+    sed -i -r "s/#ID_ACERVO/$SYNC_ID_ACERVO/g" /config/config.xml
+    sed -i -r "s/#NOME_ACERVO/$SYNC_NOME_ACERVO/g" /config/config.xml
+    echo "=======" "Adding authentication and setting permissions" "====="
+    syncthing generate --home=/config --gui-user=${ADMIN_LOGIN} --gui-password=${ADMIN_PASSWORD}
+    mkdir -p /data
+    chown -R 1000:1000 /config
+    chown -R 1000:1000 /data
+    touch "$file"
+fi
+
+if [ ! -w "$file" ]; then
+    echo "Firstboot already happened"
+    exit 1
+fi
