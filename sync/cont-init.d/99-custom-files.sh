e19d4a06a2191771ecf94c4aeeead03166ac751c
Luandro <luandro@digital-democracy.org>
2023-11-08 11:59:02 -0300

    chore: better syncthing init script; better adguard config

--- sync/cont-init.d/99-custom-files.sh
+++ sync/cont-init.d/99-custom-files.sh
@@ -6,13 +6,17 @@ file=/config/firstboot
 if [ ! -e "$file" ]; then
     echo "First run!"
     echo "================================"
-    echo "=======" "Adding variables" "====="
-    sed -i -r "s/#ID_PRINCIPAL/$SYNC_ID_PRINCIPAL/g" /config/config.xml
-    sed -i -r "s/#NOME_PRINCIPAL/$SYNC_NOME_PRINCIPAL/g" /config/config.xml
-    sed -i -r "s/#ID_ACERVO/$SYNC_ID_ACERVO/g" /config/config.xml
-    sed -i -r "s/#NOME_ACERVO/$SYNC_NOME_ACERVO/g" /config/config.xml
-    echo "=======" "Adding authentication and setting permissions" "====="
-    syncthing generate --home=/config --gui-user=${ADMIN_LOGIN} --gui-password=${ADMIN_PASSWORD}
+    if [[ $SYNC_ID_PRINCIPAL && $SYNC_NOME_PRINCIPAL && $SYNC_ID_ACERVO && $SYNC_NOME_ACERVO ]]; then
+        echo "=======" "Adding variables" "====="
+        sed -i -r "s/#ID_PRINCIPAL/$SYNC_ID_PRINCIPAL/g" /config/config.xml
+        sed -i -r "s/#NOME_PRINCIPAL/$SYNC_NOME_PRINCIPAL/g" /config/config.xml
+        sed -i -r "s/#ID_ACERVO/$SYNC_ID_ACERVO/g" /config/config.xml
+        sed -i -r "s/#NOME_ACERVO/$SYNC_NOME_ACERVO/g" /config/config.xml
+    fi
+    if [[ $ADMIN_LOGIN && $ADMIN_PASSWORD]]; then
+        echo "=======" "Adding authentication and setting permissions" "====="
+        syncthing generate --home=/config --gui-user=${ADMIN_LOGIN} --gui-password=${ADMIN_PASSWORD}
+    fi
     mkdir -p /data
     chown -R 1000:1000 /config
     chown -R 1000:1000 /data
